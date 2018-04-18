45a1ded0a4d67d862442a0727fd8975f0362f387
lj564 <564215385@qq.com>
2018-04-18 21:30:28 +0800

    初始化控制列表

--- float_lib/src/main/java/floatwindow/xishuang/float_lib/view/controlText.java
+++ float_lib/src/main/java/floatwindow/xishuang/float_lib/view/controlText.java
@@ -12,27 +12,36 @@ import floatwindow.xishuang.float_lib.R;
 import floatwindow.xishuang.float_lib.gifObject;
 
 public class controlText extends LinearLayout {
-    LinearLayout.LayoutParams param;
-    Pet pet;
+    private static LinearLayout.LayoutParams param;
+    private static Pet pet;
     private Context context;
+    private static String path;
     public controlText(Context context) {
         super(context);
         this.context=context;
+        String petName="";
+        init(context);
     }
-    public void addText(String text){
-        LinearLayout view = new LinearLayout(this.context);
-       // LinearLayout view = (LinearLayout)findViewById(R.id.controlMenu);
+    private void init(Context context){
+        
+    }
+    public void addText(final String text){//动态添加布局
+        //LinearLayout view = new LinearLayout(this.context);
+       LinearLayout view = (LinearLayout)findViewById(R.id.controlMenu);
         LinearLayout.LayoutParams linearlp = ( LinearLayout.LayoutParams) view.getLayoutParams();
         ViewGroup.LayoutParams lp=new ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT
         );
+        final String text_2=text;//引入到触摸事件监听器
         TextView txt=new TextView(this.context);
         txt.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 switch(motionEvent.getAction()){
-                    case MotionEvent.ACTION_DOWN:break;
+                    case MotionEvent.ACTION_DOWN:
+                        path=pet.getPetMotionPath(text_2);
+                        break;
                     default:break;
                 }
                 return true;
@@ -40,15 +49,20 @@ public class controlText extends LinearLayout {
         });
         txt.setText(text);
         txt.setLayoutParams(lp);
-        view.addView(txt,linearlp);
+        view.addView(txt,lp);
     }
-
-    public void setParam(LayoutParams param) {
+    public void getPet(String petName){
+        pet=(Pet) gifObject.pets.get(petName);
+    }
+    public void setParam(LayoutParams param) {//设置布局参数
         this.param = param;
     }
-    public String getPetpath(String petName,String petMotion){
-        pet = (Pet)gifObject.pets.get(petName);
-        String path=pet.getPetMotionPath(petMotion);
+    public String getPetpath(String petName,String petMotion) {//通过该类和动作直接获取到gif路径
+        pet = (Pet) gifObject.pets.get(petName);
+        path = pet.getPetMotionPath(petMotion);
         return path;
+    }
+    public String getPath(){//获取到gif路径
+        return this.path;
     }
 }
